8cd920c8639267267597d7602bb50436d351e70b
zetjede7 <zavli.juwantara7@rocketmail.com>
2016-03-26 17:08:46 +0700

    tes1

---
+++ polosan.ino
@@ -0,0 +1,41 @@
+#define trigPin 7
+#define echoPin 6
+#define led 13
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+ 
+}
+
+void loop() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration/2) / 29.1;
+ 
+
+  if (distance <= 30) {
+    digitalWrite(led, HIGH);
+    Serial.println("lagi ada mobil");
+}
+  else {
+    digitalWrite(led,LOW);
+  }
+
+  if (distance > 30){
+    Serial.println("Ga ada mobil");
+  }
+  else {
+    Serial.print(distance);
+    Serial.println(" cm");
+   
+  }
+  delay(500);
+}
